cb16b99989312a596a5a5686ad4569df2c1b4200
Rob Morgan <robbym@gmail.com>
2012-01-14 07:37:25 +0800

    Adding a comment on MySQL API

--- src/Phinx/Db/Adapter/MysqlAdapter.php
+++ src/Phinx/Db/Adapter/MysqlAdapter.php
@@ -110,6 +110,8 @@ class MysqlAdapter extends PdoAdapter implements AdapterInterface
      */
     public function createTable($tableName, $columns, $indexes = array(), $options = null)
     {
+        // This method is based on the MySQL docs here: http://dev.mysql.com/doc/refman/5.1/en/create-index.html
+        
         if (null === $options) {
             $options = array(
                 'engine' => 'InnoDB'
