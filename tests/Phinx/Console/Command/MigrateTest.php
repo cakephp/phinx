5150438205561bfc6b6fb89ff1f2312b94a851f3
Rob Morgan <robbym@gmail.com>
2012-01-14 13:23:59 +0800

    Testing migration execution with environment option

--- tests/Phinx/Console/Command/MigrateTest.php
+++ tests/Phinx/Console/Command/MigrateTest.php
@@ -30,6 +30,29 @@ class MigrateTest extends \PHPUnit_Framework_TestCase
         $commandTester = new CommandTester($command);
         $commandTester->execute(array('command' => $command->getName()));
         
-        $this->assertRegExp('/using migration path/', $commandTester->getDisplay());
+        $this->assertRegExp('/no environment specified/', $commandTester->getDisplay());
+    }
+    
+    public function testExecuteWithEnvironmentOption()
+    {
+        $application = new \Phinx\Console\PhinxApplication('testing');
+        $application->add(new Migrate());
+        
+        // setup dependencies
+        $config = new Config(array('foo' => 'bar'));
+        $output = new StreamOutput(fopen('php://memory', 'a', false));
+        
+        $command = $application->find('migrate');
+        
+        // mock the manager class
+        $managerStub = $this->getMock('\Phinx\Migration\Manager', array(), array($config, $output));
+        $managerStub->expects($this->once())
+                    ->method('migrate');
+        
+        $command->setManager($managerStub);
+        
+        $commandTester = new CommandTester($command);
+        $commandTester->execute(array('command' => $command->getName(), '--environment' => 'fakeenv'));
+        $this->assertRegExp('/using environment fakeenv/', $commandTester->getDisplay());
     }
 }
