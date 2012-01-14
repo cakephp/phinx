8140b04ab277bc438f7845d9f8a533b9addaa125
Rob Morgan <robbym@gmail.com>
2012-01-14 10:46:21 +0800

    Adding a test to stop phpunit from whinging

--- tests/Phinx/Migration/ManagerTest.php
+++ tests/Phinx/Migration/ManagerTest.php
@@ -2,6 +2,17 @@
 
 namespace Test\Phinx\Migration;
 
+use Symfony\Component\Console\Output\StreamOutput,
+    Phinx\Config\Config,
+    Phinx\Migration\Manager;
+
 class ManagerTest extends \PHPUnit_Framework_TestCase
 {
+    public function testInstantiation()
+    {
+        $config = new Config(array('foo' => 'bar'));
+        $output = new StreamOutput(fopen('php://memory', 'w', false));
+        $manager = new Manager($config, $output);
+        $this->assertTrue($manager->getOutput() instanceof StreamOutput);
+    }
 }
