dec2cb3e3cde33787c3aa2bd0be40e82fbeb99f3
Rob Morgan <robbym@gmail.com>
2012-03-02 22:13:01 +1100

    Declaring the manager var. tabs to spaces.

--- src/Phinx/Console/Command/AbstractCommand.php
+++ src/Phinx/Console/Command/AbstractCommand.php
@@ -3,11 +3,11 @@
 namespace Phinx\Console\Command;
 
 use Symfony\Component\Config\FileLocator,
-	Symfony\Component\Console\Command\Command,
+    Symfony\Component\Console\Command\Command,
     Symfony\Component\Console\Input\InputInterface,
     Symfony\Component\Console\Input\InputArgument,
     Symfony\Component\Console\Output\OutputInterface,
-	Phinx\Config\Config,
+    Phinx\Config\Config,
     Phinx\Migration\Manager,
     Phinx\Adapter\AdapterInterface;
 
@@ -18,99 +18,82 @@ use Symfony\Component\Config\FileLocator,
  */
 abstract class AbstractCommand extends Command
 {
-	/**
-	 * @var ArrayAccess
-	 */
-	protected $config;
-	
-	/**
-	 * @var \Phinx\Adapter\AdapterInterface
-	 */
-	protected $adapter;
-	
-	/**
-	 * {@inheritdoc}
-	 */
-	protected function configure()
-	{
-		$this->addOption('--configuration', '-c', InputArgument::OPTIONAL, 'The configuration file to load');
-	}
-	
-	/**
-	 * Bootstrap Phinx.
-	 *
-	 * @return void
-	 */
-	public function bootstrap(InputInterface $input, OutputInterface $output)
-	{
-		/**
-		 * Bootstrap
-		 */
-		$configFile = $input->getOption('configuration');
-		
-		if (null === $configFile) {
-			$configFile = 'phinx.yml';
-		}
-		
-		$cwd = getcwd();
-		
-		// locate the phinx config file (default: phinx.yml)
-		// TODO - In future walk the tree in reverse (max 10 levels)
-		$locator = new FileLocator(array(
-			$cwd . DIRECTORY_SEPARATOR
-		));
-		
-		// Locate() throws an exception if the file does not exist
-		$configFilePath = $locator->locate($configFile, $cwd, $first = true);
+    /**
+     * @var ArrayAccess
+     */
+    protected $config;
+    
+    /**
+     * @var \Phinx\Adapter\AdapterInterface
+     */
+    protected $adapter;
+    
+    /**
+     * @var \Phinx\Migration\Manager;
+     */
+    protected $manager;
+    
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this->addOption('--configuration', '-c', InputArgument::OPTIONAL, 'The configuration file to load');
+    }
+    
+    /**
+     * Bootstrap Phinx.
+     *
+     * @return void
+     */
+    public function bootstrap(InputInterface $input, OutputInterface $output)
+    {
+        /**
+         * Bootstrap
+         */
+        $configFile = $input->getOption('configuration');
+        
+        if (null === $configFile) {
+            $configFile = 'phinx.yml';
+        }
+        
+        $cwd = getcwd();
+        
+        // locate the phinx config file (default: phinx.yml)
+        // TODO - In future walk the tree in reverse (max 10 levels)
+        $locator = new FileLocator(array(
+            $cwd . DIRECTORY_SEPARATOR
+        ));
+        
+        // Locate() throws an exception if the file does not exist
+        $configFilePath = $locator->locate($configFile, $cwd, $first = true);
 
-		$output->writeln('<info>using config</info> .' . str_replace(getcwd(), '', $configFilePath));
-		
-		// parse the config file and load it into the config object
-		$this->setConfig(Config::fromYaml($configFilePath));
+        $output->writeln('<info>using config</info> .' . str_replace(getcwd(), '', $configFilePath));
+        
+        // parse the config file and load it into the config object
+        $this->setConfig(Config::fromYaml($configFilePath));
 
-		// report the migrations path
-		$output->writeln('<info>using migration path</info> ' . $this->getConfig()->getMigrationPath());
-	
-		if (null === $this->getManager()) {
-    		// load the migrations manager and inject the config
-	    	$manager = new Manager($this->getConfig(), $output);
-		    $this->setManager($manager);
-		}
-	}
-	
-	/**
-	 * Sets the migration manager.
-	 *
-	 * @param Manager $manager
-	 * @return AbstractCommand
-	 */
-	public function setManager(Manager $manager)
-	{
-		$this->manager = $manager;
-		return $this;
-	}
-	
-	/**
-	 * Gets the migration manager.
-	 *
-	 * @return \Manager
-	 */
-	public function getManager()
-	{
-		return $this->manager;
-	}
+        // report the migrations path
+        $output->writeln('<info>using migration path</info> ' . $this->getConfig()->getMigrationPath());
+    
+        if (null === $this->getManager()) {
+            // load the migrations manager and inject the config
+            $manager = new Manager($this->getConfig(), $output);
+            $this->setManager($manager);
+        }
+    }
 
-	/**
-	 * Sets the config.
-	 *
-	 * @param \ArrayAccess $config
-	 * @return AbstractCommand
-	 */
-	public function setConfig(\ArrayAccess $config)
-	{
-		$this->config = $config;
-		return $this;
-	}
+    /**
+     * Sets the config.
+     *
+     * @param \ArrayAccess $config
+     * @return AbstractCommand
+     */
+    public function setConfig(\ArrayAccess $config)
+    {
+        $this->config = $config;
+        return $this;
+    }
 
     /**
      * Gets the config.
@@ -121,8 +104,8 @@ abstract class AbstractCommand extends Command
     {
         return $this->config;
     }
-	
-	/**
+    
+    /**
      * Sets the database adapter.
      *
      * @param AdapterInterface $adapter
@@ -142,5 +125,27 @@ abstract class AbstractCommand extends Command
     public function getAdapter()
     {
         return $this->adapter;
+    }
+    
+    /**
+     * Sets the migration manager.
+     *
+     * @param Manager $manager
+     * @return AbstractCommand
+     */
+    public function setManager(Manager $manager)
+    {
+        $this->manager = $manager;
+        return $this;
+    }
+    
+    /**
+     * Gets the migration manager.
+     *
+     * @return \Manager
+     */
+    public function getManager()
+    {
+        return $this->manager;
     }
 }
