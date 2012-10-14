7fdc7c961b5448dfce5c750d5511f1a9c97f7cde
Rob Morgan <robbym@gmail.com>
2012-10-14 21:53:44 +1100

    tabs to spaces

--- tests/Phinx/Db/Adapter/PdoAdapterTest.php
+++ tests/Phinx/Db/Adapter/PdoAdapterTest.php
@@ -10,24 +10,24 @@ class PdoAdapterTest extends \PHPUnit_Framework_TestCase
     
     public function setUp()
     {
-		$this->adapter = $this->getMockForAbstractClass('\Phinx\Db\Adapter\PdoAdapter', array(array('foo' => 'bar')));
+        $this->adapter = $this->getMockForAbstractClass('\Phinx\Db\Adapter\PdoAdapter', array(array('foo' => 'bar')));
     }
-	
-	public function tearDown()
-	{
-		unset($this->adapter);
-	}
-	
+    
+    public function tearDown()
+    {
+        unset($this->adapter);
+    }
+    
     public function testOptions()
     {
-		$options = $this->adapter->getOptions();
-		$this->assertArrayHasKey('foo', $options);
-		$this->assertEquals('bar', $options['foo']);
-	}
-	
-	public function testSchemaTableName()
-	{
-		$this->adapter->setSchemaTableName('schema_table_test');
-		$this->assertEquals('schema_table_test', $this->adapter->getSchemaTableName());
-	}
+        $options = $this->adapter->getOptions();
+        $this->assertArrayHasKey('foo', $options);
+        $this->assertEquals('bar', $options['foo']);
+    }
+    
+    public function testSchemaTableName()
+    {
+        $this->adapter->setSchemaTableName('schema_table_test');
+        $this->assertEquals('schema_table_test', $this->adapter->getSchemaTableName());
+    }
 }
