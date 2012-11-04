a5d6b445536c618466a87fc048641f2dcb5e2c42
Rob Morgan <robbym@gmail.com>
2012-11-04 13:43:12 +1100

    Modifying the test command to only test one env at a time

--- src/Phinx/Console/Command/Test.php
+++ src/Phinx/Console/Command/Test.php
@@ -1,7 +1,4 @@
 <?php
-/**
- * @author Leonid Kuzmin <lndkuzmin@gmail.com>
- */
 
 namespace Phinx\Console\Command;
 
@@ -10,6 +7,9 @@ use Symfony\Component\Console\Input\InputInterface,
     Symfony\Component\Console\Input\InputOption,
     Symfony\Component\Console\Output\OutputInterface;
 
+/**
+ * @author Leonid Kuzmin <lndkuzmin@gmail.com>
+ */
 class Test extends AbstractCommand
 {
     /**
@@ -18,13 +18,17 @@ class Test extends AbstractCommand
     protected function configure()
     {
         parent::configure();
+        
+        $this->addOption('--environment', '-e', InputArgument::OPTIONAL, 'The target environment');
 
         $this->setName('test')
-            ->setDescription('Verify configuration file')
-            ->setHelp(<<<EOT
-The <info>test</info> command verifies the YAML configuration file and prints errors if any
+             ->setDescription('Verify configuration file')
+             ->setHelp(<<<EOT
+The <info>test</info> command verifies the YAML configuration file and optionally an environment
 
 <info>phinx test</info>
+<info>phinx test -e development</info>
+
 EOT
         );
     }
@@ -34,7 +38,6 @@ EOT
      *
      * @param \Symfony\Component\Console\Input\InputInterface   $input
      * @param \Symfony\Component\Console\Output\OutputInterface $output
-     *
      * @return void
      */
     protected function execute(InputInterface $input, OutputInterface $output)
@@ -44,27 +47,24 @@ EOT
 
         $migrationsPath = $this->getConfig()->getMigrationPath();
 
-        //validate if migrations path is valid
-        if(!file_exists($migrationsPath)) {
-            throw new \RuntimeException("Migrations path is invalid");
+        // validate if migrations path is valid
+        if (!file_exists($migrationsPath)) {
+            throw new \RuntimeException('The migrations path is invalid');
         }
 
-        $environments = $this->getConfig()->getEnvironments();
-
-        if(count($environments)) {
-            //validate if default environment is set
-            $output->writeln('<info>validating default environment</info>');
-            $defaultEnvironment = $this->getConfig()->getDefaultEnvironment();
-
-            //validate environments
-            foreach($environments as $name => $env) {
-                $output->writeln(sprintf("<info>validating environment '%s'</info>", $name));
-                $environment = new \Phinx\Migration\Manager\Environment($name, $this->getConfig()->getEnvironment($name));
-                //validate environment connection
-                $environment->getAdapter()->connect();
+        $envName = $input->getOption('environment');
+        if ($envName) {
+            if (!$this->getConfig()->hasEnvironment($envName)) {
+                throw new \InvalidArgumentException(sprintf(
+                    'The environment "%s" does not exist',
+                    $envName
+                ));
             }
-        } else {
-            throw new \RuntimeException("No configuration environments found");
+            
+            $output->writeln(sprintf('<info>validating environment</info> %s', $envName));
+            $environment = new \Phinx\Migration\Manager\Environment($envName, $this->getConfig()->getEnvironment($envName));
+            // validate environment connection
+            $environment->getAdapter()->connect();
         }
 
         $output->writeln('<info>success!</info>');
