34309a20c76576845c70bd4b4b82e57067019074
Nikolay Galkin <mr.galkin@gmail.com>
2013-05-24 16:52:38 +0300

    Changed carets to UNIX format. Takeout verbose option

--- src/Phinx/Console/Command/Migrate.php
+++ src/Phinx/Console/Command/Migrate.php
@@ -22,7 +22,7 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
  * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
  * IN THE SOFTWARE.
- * 
+ *
  * @package    Phinx
  * @subpackage Phinx\Console
  */
@@ -33,7 +33,7 @@ use Symfony\Component\Console\Command\Command,
     Symfony\Component\Console\Input\InputArgument,
     Symfony\Component\Console\Input\InputOption,
     Symfony\Component\Console\Output\OutputInterface;
-    
+
 class Migrate extends AbstractCommand
 {
     /**
@@ -42,10 +42,9 @@ class Migrate extends AbstractCommand
      protected function configure()
      {
          parent::configure();
-         
+
          $this->addOption('--environment', '-e', InputArgument::OPTIONAL, 'The target environment');
-         $this->addOption('--verbose', '-v', InputOption::VALUE_NONE, 'Show more output');
-         
+
          $this->setName('migrate')
               ->setDescription('Migrate the database')
               ->addOption('--target', '-t', InputArgument::OPTIONAL, 'The version number to migrate to')
@@ -62,24 +61,24 @@ EOT
 
     /**
      * Migrate the database.
-     * 
+     *
      * @return void
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $this->bootstrap($input, $output);
-        
+
         $version = $input->getOption('target');
         $environment = $input->getOption('environment');
         $isVerbose = (bool) $input->getOption('verbose');
-        
+
         if (null === $environment) {
             $environment = $this->getConfig()->getDefaultEnvironment();
             $output->writeln('<comment>warning</comment> no environment specified, defaulting to: ' . $environment);
         } else {
             $output->writeln('<info>using environment</info> ' . $environment);
         }
-        
+
         $envOptions = $this->getConfig()->getEnvironment($environment);
         $output->writeln('<info>using adapter</info> ' . $envOptions['adapter']);
         $output->writeln('<info>using database</info> ' . $envOptions['name']);
@@ -93,7 +92,7 @@ EOT
         $start = microtime(true);
         $this->getManager()->migrate($environment, $version);
         $end = microtime(true);
-        
+
         $output->writeln('');
         $output->writeln('<comment>All Done. Took ' . sprintf('%.4fs', $end - $start) . '</comment>');
     }
