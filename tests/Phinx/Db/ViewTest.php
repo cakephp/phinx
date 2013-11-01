ff7ffd8ed7085aef17a5650dde381aa39b0f5f9b
Daniel Yule <daniel.yule@gmail.com>
2013-11-01 12:25:16 -0300

    Fixed another test case to be compatible with PHP 5.4.

--- tests/Phinx/Db/ViewTest.php
+++ tests/Phinx/Db/ViewTest.php
@@ -12,16 +12,17 @@ class ViewTest extends \PHPUnit_Framework_TestCase {
 
         $view = new \Phinx\Db\View("myName", $adapter);
 
+        $condition = new Condition();
 
         $view->
-            setCondition( (new Condition())
-        ->aand()
-            ->comparison("=")
-                ->column("col1", "table1")
-                ->column("col2", "table2")
-            ->comparison("<")
-                ->column("col3", "table1")
-                ->column("col4"));
+            setCondition( $condition
+                ->aand()
+                    ->comparison("=")
+                        ->column("col1", "table1")
+                        ->column("col2", "table2")
+                    ->comparison("<")
+                        ->column("col3", "table1")
+                        ->column("col4"));
 
         $this->assertEquals("`table1`.`col1` = `table2`.`col2` AND `table1`.`col3` < `col4`",
             $view->getCondition()->getConditionSQL($adapter));
