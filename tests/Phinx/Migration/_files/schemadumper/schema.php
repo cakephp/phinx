c9374047b93e5eb0577aa40902ff29b6c26135a8
Denis Tukalenko <detook@gmail.com>
2013-12-15 23:01:50 +0200

    update unit tests to handle '`' quoting

--- tests/Phinx/Migration/_files/schemadumper/schema.php
+++ tests/Phinx/Migration/_files/schemadumper/schema.php
@@ -6,30 +6,30 @@ class Schema extends AbstractMigration
 {
     public function up()
     {
-        $this->table('one')
-            ->addColumn('name', 'string', array('length'=>50))
-            ->addColumn('dummy_int', 'integer')
-            ->addColumn('dummy_int_null', 'integer', array('null'=>true))
+        $this->table('`one`')
+            ->addColumn('`name`', 'string', array('length'=>50))
+            ->addColumn('`dummy_int`', 'integer')
+            ->addColumn('`dummy_int_null`', 'integer', array('null'=>true))
             ->save();
 
-        $this->table('two')
-            ->addColumn('name', 'string', array('length'=>50))
-            ->addColumn('dummy_int', 'integer')
-            ->addColumn('dummy_int_null', 'integer', array('null'=>true))
+        $this->table('`two`')
+            ->addColumn('`name`', 'string', array('length'=>50))
+            ->addColumn('`dummy_int`', 'integer')
+            ->addColumn('`dummy_int_null`', 'integer', array('null'=>true))
             ->save();
 
-        $this->table('three')
-            ->addColumn('name', 'string', array('length'=>50))
-            ->addColumn('dummy_int', 'integer')
-            ->addColumn('dummy_int_null', 'integer', array('null'=>true))
+        $this->table('`three`')
+            ->addColumn('`name`', 'string', array('length'=>50))
+            ->addColumn('`dummy_int`', 'integer')
+            ->addColumn('`dummy_int_null`', 'integer', array('null'=>true))
             ->save();
 
     }
 
     public function down()
     {
-        $this->dropTable('one');
-        $this->dropTable('two');
-        $this->dropTable('three');
+        $this->dropTable('`one`');
+        $this->dropTable('`two`');
+        $this->dropTable('`three`');
     }
 }
