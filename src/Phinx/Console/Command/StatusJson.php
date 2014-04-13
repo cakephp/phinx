698795a0a139523a96d92b172a062128fbc7e131
Rodrigo <rsdevigo@gmail.com>
2014-04-13 11:47:55 -0300

    ** Create a new command
    This command works like phinx status, but with a difference, it print on the screen a json object like this:
    
    [
    {"status": "up/down", "migration_id": "", "migration_name" : ""},
    {"status": "up/down", "migration_id": "", "migration_name" : ""}
    ]
    
    Motivation to do this: Sometime u execute phinx in a CI tool or in a Deploy script and with the default output it´s hard to get the list of migrations.

---
+++ src/Phinx/Console/Command/StatusJson.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * Phinx
+ *
+ * (The MIT license)
+ * Copyright (c) 2014 Rob Morgan
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated * documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ * 
+ * @package    Phinx
+ * @subpackage Phinx\Console
+ */
+namespace Phinx\Console\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class StatusJson extends AbstractCommand
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        parent::configure();
+         
+        $this->addOption('--environment', '-e', InputArgument::OPTIONAL, 'The target environment');
+         
+        $this->setName('statusjson')
+             ->setDescription('Show migration status and return this in a json object')
+             ->setHelp(
+<<<EOT
+The <info>statusjson</info> command prints a list of all migrations, along with their current status in a format json
+
+<info>phinx statusjson -e development</info>
+EOT
+             );
+    }
+
+    /**
+     * Show the migration status.
+     * 
+     * @return void
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->bootstrap($input, $output);
+        
+        $environment = $input->getOption('environment');
+        
+        if (null === $environment) {
+            $environment = $this->getConfig()->getDefaultEnvironment();
+            $output->writeln('<comment>warning</comment> no environment specified, defaulting to: ' . $environment);
+        } else {
+            $output->writeln('<info>using environment</info> ' . $environment);
+        }
+        
+        // print the status
+        $this->getManager()->printStatus($environment, true);
+    }
+}
