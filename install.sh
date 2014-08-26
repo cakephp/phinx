94e2685260ff3d4c524d3064ae41cc85ee26f6a6
Victor Bjelkholm <victor@bjelkholm.com>
2014-08-26 12:19:19 +0200

    Simplify, simplify and simplify

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #! /bin/bash
+
 # Script to install Phinx
 # Requirements: php, curl, phar
+
+requirements=(git php phar curl)
 
 command_exists () {
 	type "$1" &> /dev/null ;
@@ -14,53 +17,47 @@ exists_message () {
 	echo "$1 existed"
 }
 
-# TODO needs to be nicer
-if command_exists git; then
-	exists_message "Git"
-	if command_exists php; then
-		exists_message "PHP"
-		if command_exists phar; then
-			exists_message "phar"
-			if command_exists curl; then
-				exists_message "Curl"
-				echo "Beginning installation"
-				# Create temporary install folder
-				mkdir /tmp/phinx_install
-				cd /tmp/phinx_install
-				# Clone phinx
-				git clone git://github.com/robmorgan/phinx.git
-				cd phinx
-				# Install Composer
-				curl -s https://getcomposer.org/installer | php
-				# Install Phinx dependencies
-				php composer.phar install
-				# Install Box
-				curl -s http://box-project.org/installer.php | php
-				# Create Phar archive
-				php box.phar build
-				# Move to executable path
-				if mv phinx*.phar /usr/bin/phinx; then
-					echo "## Installation complete!"
-				else
-					echo "Trying moving with sudo"
-					if sudo mv phinx*.phar /usr/bin/phinx; then
-						echo "## Installation complete!"
-					else
-						echo "Failed with installing to selected directory..."
-						exit
-					fi
-				fi
-				# Remove temporary install folder
-				#rm -rf /tmp/phinx_install
-			else
-				need_message "curl"
-			fi
-		else
-			need_message "phar"
-		fi
+
+for i in ${requirements[@]}; do
+	if command_exists "$i"; then
+		exists_message "$i"
 	else
-		need_message "PHP"
+		need_message "$i"
+		exit 1
+	fi
+done
+
+echo "Beginning installation"
+# Create temporary install folder
+mkdir /tmp/phinx_install
+cd /tmp/phinx_install
+# Clone phinx
+git clone git://github.com/robmorgan/phinx.git
+cd phinx
+# Install Composer
+curl -s https://getcomposer.org/installer | php
+# Install Phinx dependencies
+php composer.phar install --no-dev
+# Install Box
+curl -LSs http://box-project.org/installer.php | php
+# Create Phar archive
+if php box.phar build; then
+	# Move to executable path
+	# TODO needs to be changable
+	if mv phinx*.phar /usr/bin/phinx; then
+		echo "## Installation complete!"
+	else
+		echo "Trying moving with sudo"
+		# TODO needs to be changable
+		if sudo mv phinx*.phar /usr/bin/phinx; then
+			echo "## Installation complete!"
+		else
+			echo "Failed with installing to selected directory..."
+			exit
+		fi
 	fi
 else
-	need_message "Git"
+	echo "## Building the box failed, please read the error and retry installation once fixed"
 fi
+# Remove temporary install folder
+rm -rf /tmp/phinx_install
