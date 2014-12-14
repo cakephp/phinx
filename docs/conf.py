9096024ab7d6985ae541eb09aaf480e8f95a342c
Rob Morgan <robbym@gmail.com>
2014-12-14 14:06:08 +0100

    update to 0.4

--- docs/conf.py
+++ docs/conf.py
@@ -48,7 +48,7 @@ copyright = u'2014, Rob Morgan'
 # built documents.
 #
 # The short X.Y version.
-version = '0.3'
+version = '0.4'
 # The full version, including alpha/beta/rc tags.
 release = '0.4.0'
 
