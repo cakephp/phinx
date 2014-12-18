a11c8ceb1d90b40c80238f3fa534b7cc1f40a374
Richard Quadling <RQuadling@GMail.com>
2014-12-18 19:21:59 +0000

    Provide options to use an alternative template file or a class name that will implement a CreationInterface allowing for more control once the template has been saved.

--- src/Phinx/Console/Command/Create.php
+++ src/Phinx/Console/Command/Create.php
@@ -28,9 +28,11 @@
  */
 namespace Phinx\Console\Command;
 
+use Phinx\Migration\CreationInterface;
 use Phinx\Migration\Util;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class Create extends AbstractCommand
@@ -50,6 +52,13 @@ class Create extends AbstractCommand
                 PHP_EOL,
                 PHP_EOL
             ));
+
+        // An alternative template.
+        $this->addOption('template', 't', InputOption::VALUE_REQUIRED, 'Use an alternative template');
+
+        // A classname to be used to gain access to the template content as well as the ability to
+        // have a callback once the migration file has been created.
+        $this->addOption('class', 'l', InputOption::VALUE_REQUIRED, 'Use a class implementing Phinx\Migration\CreationInterface to generate the template');
     }
 
     /**
@@ -63,6 +72,10 @@ class Create extends AbstractCommand
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        /**
+         * @var CreationInterface $creationClass
+         */
+
         $this->bootstrap($input, $output);
 
         // get the migration path from the config
@@ -96,8 +109,53 @@ class Create extends AbstractCommand
             ));
         }
 
-        // load the migration template
-        $contents = file_get_contents(dirname(__FILE__) . '/../../Migration/Migration.template.php.dist');
+        // Get the alternative template and static class options, but only allow one of them.
+        $altTemplate = $input->getOption('template');
+        $creationClassName = $input->getOption('class');
+        if (!empty($altTemplate) && !empty($creationClassName)){
+            throw new \InvalidArgumentException('Cannot use --template and --class');
+        }
+
+        // Verify the alternative template file's existence.
+        if (!empty($altTemplate) &&!file_exists($altTemplate)){
+            throw new \InvalidArgumentException(sprintf(
+                'The alternative template file "%s" does not exist',
+                $altTemplate
+            ));
+        }
+
+        // Verify the static class exists and that it implements the required interface.
+        if (!empty($creationClassName)) {
+            if (!class_exists($creationClassName, true)) {
+                throw new \InvalidArgumentException(
+                    sprintf(
+                        'The class "%s" does not exist',
+                        $creationClassName
+                    )
+                );
+            }
+            if (!is_subclass_of($creationClassName, '\Phinx\Migration\CreationInterface', true)) {
+                throw new \InvalidArgumentException(
+                    sprintf(
+                        'The class "%s" does not implement the required interface "\Phinx\Migration\CreationInterface"',
+                        $creationClassName
+                    )
+                );
+            }
+        }
+
+        // Determine the appropriate mechanism to get the template
+        if (!empty($altTemplate)) {
+            // Get the template from the alternative template filename.
+            $contents = file_get_contents($altTemplate);
+        } elseif (!empty($creationClassName)) {
+            // Get the template from the creation class
+            $creationClass = new $creationClassName();
+            $contents = $creationClass->getMigrationTemplate();
+        } else {
+            // load the migration template
+            $contents = file_get_contents(dirname(__FILE__) . '/../../Migration/Migration.template.php.dist');
+        }
 
         // inject the class name
         $contents = str_replace('$className', $className, $contents);
@@ -110,6 +168,20 @@ class Create extends AbstractCommand
                 'The file "%s" could not be written to',
                 $path
             ));
+        }
+
+        // Do we need to do the post creation call to the creation class?
+        if (!empty($creationClassName)) {
+            try {
+                $creationClass->postMigrationCreation($filePath, $className, $this->getConfig()->getMigrationBaseClassName());
+            }
+            catch(Exception $ex){
+                throw new \RuntimeException(sprintf(
+                    'Problem calling $s->postMigrationCreation(), resulting in $s',
+                    $creationClassName,
+                    $ex->getMessage()
+                ));
+            }
         }
 
         $output->writeln('<info>created</info> .' . str_replace(getcwd(), '', $filePath));
