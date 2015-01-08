f8a2ea1ee49f1536a564d4855aef3db446675d8f
Richard Quadling <RQuadling@GMail.com>
2015-01-08 16:00:48 +0000

    No need to interact with the config more than we need to.

--- src/Phinx/Console/Command/Create.php
+++ src/Phinx/Console/Command/Create.php
@@ -114,7 +114,7 @@ class Create extends AbstractCommand
             ));
         }
 
-        $output->writeln('<info>using migration base class</info> ' . $this->getConfig()->getMigrationBaseClassName(false));
+        $output->writeln('<info>using migration base class</info> ' . $classes['$useClassName']);
         $output->writeln('<info>created</info> .' . str_replace(getcwd(), '', $filePath));
     }
 }
