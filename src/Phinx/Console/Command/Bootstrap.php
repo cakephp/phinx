3a84ba9d009294fae83ad58a21c55253a90646d5
JR Wilbert <rwilbert@digitecinteractive.com>
2015-09-16 13:00:11 -0400

    added bootstrap command

---
+++ src/Phinx/Console/Command/Bootstrap.php
@@ -0,0 +1,105 @@
+<?php
+/**
+ * Phinx
+ *
+ * (The MIT license)
+ * Copyright (c) 2015 Rob Morgan
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated * documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ *
+ * @package    Phinx
+ * @subpackage Phinx\Console
+ */
+namespace Phinx\Console\Command;
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class Bootstrap extends AbstractCommand
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        parent::configure();
+
+        $this->addOption('--environment', '-e', InputOption::VALUE_REQUIRED, 'The target environment');
+
+        $this->setName('bootstrap')
+            ->setDescription('Runs the bootstrap SQL script')
+            ->addOption('--force', '-f', InputOption::VALUE_NONE, 'Force run the bootstrap script. ')
+            ->addOption('--source', '-s', InputOption::VALUE_REQUIRED, 'Path to bootstrap script.')
+            ->setHelp(
+                <<<EOT
+                The <info>Bootstrap</info> command initializes the database. It should run before any migrations
+
+<info>phinx bootstrap -e development</info>
+
+EOT
+            );
+    }
+
+    /**
+     * Rollback the migration.
+     *
+     * @param InputInterface  $input
+     * @param OutputInterface $output
+     *
+     * @return void
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->bootstrap($input, $output);
+
+        $environment = $input->getOption('environment');
+        $force = ($input->getOption('force')) ? true : false;
+        $source = ($input->getOption('source')) ? $input->getOption('source') : null;
+
+
+        if (null === $environment) {
+            $environment = $this->getConfig()->getDefaultEnvironment();
+            $output->writeln('<comment>warning</comment> no environment specified, defaulting to: ' . $environment);
+        } else {
+            $output->writeln('<info>using environment</info> ' . $environment);
+        }
+
+        $envOptions = $this->getConfig()->getEnvironment($environment);
+        if (isset($envOptions['adapter'])) {
+            $output->writeln('<info>using adapter</info> ' . $envOptions['adapter']);
+        }
+
+        if (isset($envOptions['wrapper'])) {
+            $output->writeln('<info>using wrapper</info> ' . $envOptions['wrapper']);
+        }
+
+        if (isset($envOptions['name'])) {
+            $output->writeln('<info>using database</info> ' . $envOptions['name']);
+        }
+
+        // rollback the specified environment
+        $start = microtime(true);
+        $this->getManager()->executeBootstrap($environment, $force, $source);
+        $end = microtime(true);
+
+        $output->writeln('');
+        $output->writeln('<comment>All Done. Took ' . sprintf('%.4fs', $end - $start) . '</comment>');
+    }
+}
