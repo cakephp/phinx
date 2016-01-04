88637000c4c391da98980324e4f7a9b785132c05
Richard Quadling <RQuadling@GMail.com>
2016-01-04 14:07:49 +0000

    Add unit tests to test when both template and class parameters or config are used

--- tests/Phinx/Console/Command/CreateTest.php
+++ tests/Phinx/Console/Command/CreateTest.php
@@ -57,4 +57,58 @@ class CreateTest extends \PHPUnit_Framework_TestCase
         sleep(1.01); // need at least a second due to file naming scheme
         $commandTester->execute(array('command' => $command->getName(), 'name' => 'MyDuplicateMigration'));
     }
+
+
+    /**
+     * @expectedException \Exception
+     * @expectedExceptionMessage Cannot use --template and --class at the same time
+     */
+    public function testSupplyingBothClassAndTemplateAtCommandLineThrowsException()
+    {
+        $application = new \Phinx\Console\PhinxApplication('testing');
+        $application->add(new Create());
+
+        // setup dependencies
+        $output = new StreamOutput(fopen('php://memory', 'a', false));
+
+        $command = $application->find('create');
+
+        // mock the manager class
+        $managerStub = $this->getMock('\Phinx\Migration\Manager', array(), array($this->config, $output));
+
+        $command->setConfig($this->config);
+        $command->setManager($managerStub);
+
+        $commandTester = new CommandTester($command);
+        $commandTester->execute(array('command' => $command->getName(), 'name' => 'MyFailingMigration', '--template' => 'MyTemplate', '--class' => 'MyTemplateClass'));
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     * @expectedExceptionMessage Cannot define template:class and template:file at the same time
+     */
+    public function testSupplyingBothClassAndTemplateInConfigThrowsException()
+    {
+        $application = new \Phinx\Console\PhinxApplication('testing');
+        $application->add(new Create());
+
+        // setup dependencies
+        $output = new StreamOutput(fopen('php://memory', 'a', false));
+
+        $command = $application->find('create');
+
+        // mock the manager class
+        $managerStub = $this->getMock('\Phinx\Migration\Manager', array(), array($this->config, $output));
+
+        $this->config['templates'] = array(
+            'file' => 'MyTemplate',
+            'class' => 'MyTemplateClass'
+        );
+
+        $command->setConfig($this->config);
+        $command->setManager($managerStub);
+
+        $commandTester = new CommandTester($command);
+        $commandTester->execute(array('command' => $command->getName(), 'name' => 'MyFailingMigration'));
+    }
 }
