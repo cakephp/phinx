1cac3239b1e2b42e7cb704289440f02cbbf6d88a
Igor Santos <igorsantos07@gmail.com>
2016-08-22 22:36:31 -0300

    Adding hint on migration name in CamelCase

--- src/Phinx/Console/Command/Create.php
+++ src/Phinx/Console/Command/Create.php
@@ -52,7 +52,7 @@ class Create extends AbstractCommand
 
         $this->setName('create')
             ->setDescription('Create a new migration')
-            ->addArgument('name', InputArgument::REQUIRED, 'What is the name of the migration?')
+            ->addArgument('name', InputArgument::REQUIRED, 'What is the name of the migration (in CamelCase)?')
             ->setHelp(sprintf(
                 '%sCreates a new database migration%s',
                 PHP_EOL,
