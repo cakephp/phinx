0a1b13a8ec3fb5366c1216cf7222560b392c18bb
Rob Morgan <robbym@gmail.com>
2017-01-21 14:12:41 +0100

    fix tests on windows

--- tests/Phinx/Console/Command/AbstractCommandTest.php
+++ tests/Phinx/Console/Command/AbstractCommandTest.php
@@ -12,7 +12,7 @@ class AbstractCommandTest extends \PHPUnit_Framework_TestCase
         chdir(__DIR__ . '/Asset/default-test');
         $command = new Asset\FindConfigTestCommand();
         $foundFile = $command->callLocateConfigFile();
-        $this->assertEquals(__DIR__ . '/Asset/default-test/phinx.yml', $foundFile);
+        $this->assertEquals(__DIR__ . DIRECTORY_SEPARATOR . implode(DIRECTORY_SEPARATOR, ['Asset', 'default-test', 'phinx.yml']), $foundFile);
     }
 
     public function testFindFileInCurrentWorkingDirectory()
@@ -20,7 +20,7 @@ class AbstractCommandTest extends \PHPUnit_Framework_TestCase
         putenv('PHINX_CONFIG_DIR=' . __DIR__ . '/Asset/env-test');
         $command = new Asset\FindConfigTestCommand();
         $foundFile = $command->callLocateConfigFile();
-        $this->assertEquals(__DIR__ . '/Asset/env-test/phinx.php', $foundFile);
+        $this->assertEquals(__DIR__ . DIRECTORY_SEPARATOR . implode(DIRECTORY_SEPARATOR, ['Asset', 'env-test', 'phinx.php']), $foundFile);
     }
 
     public function testFindFileInNestedConfigDirectory()
@@ -28,7 +28,7 @@ class AbstractCommandTest extends \PHPUnit_Framework_TestCase
         chdir(__DIR__ . '/Asset/app-test');
         $command = new Asset\FindConfigTestCommand();
         $foundFile = $command->callLocateConfigFile();
-        $this->assertEquals(__DIR__ . '/Asset/app-test/config/phinx.php', $foundFile);
+        $this->assertEquals(__DIR__ . DIRECTORY_SEPARATOR . implode(DIRECTORY_SEPARATOR, ['Asset', 'app-test', 'config', 'phinx.php']), $foundFile);
     }
 
 }
