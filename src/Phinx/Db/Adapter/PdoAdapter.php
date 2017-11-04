d587ad646f8485d58f5721ef02581d5e23c34d89
ipoz <rafal@ipoz.eu>
2017-11-04 12:15:55 +0100

    Fixed issue #575
    Insert into phinxlog with many schemes

--- src/Phinx/Db/Adapter/PdoAdapter.php
+++ src/Phinx/Db/Adapter/PdoAdapter.php
@@ -264,7 +264,7 @@ abstract class PdoAdapter extends AbstractAdapter
             // up
             $sql = sprintf(
                 "INSERT INTO %s (%s, %s, %s, %s, %s) VALUES ('%s', '%s', '%s', '%s', %s);",
-                $this->getSchemaTableName(),
+                $this->quoteTableName($this->getSchemaTableName()),
                 $this->quoteColumnName('version'),
                 $this->quoteColumnName('migration_name'),
                 $this->quoteColumnName('start_time'),
@@ -282,7 +282,7 @@ abstract class PdoAdapter extends AbstractAdapter
             // down
             $sql = sprintf(
                 "DELETE FROM %s WHERE %s = '%s'",
-                $this->getSchemaTableName(),
+                $this->quoteTableName($this->getSchemaTableName()),
                 $this->quoteColumnName('version'),
                 $migration->getVersion()
             );
