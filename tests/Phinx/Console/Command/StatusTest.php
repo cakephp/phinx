0b3f73cd3b8f034c1629a32cfb078652a55635fc
Mike van Rooyen <michael.vanrooyen@extreme-creations.co.uk>
2017-11-23 14:17:42 +0000

    Add multidb tests to status test

--- tests/Phinx/Console/Command/StatusTest.php
+++ tests/Phinx/Console/Command/StatusTest.php
@@ -60,6 +60,48 @@ class StatusTest extends \PHPUnit_Framework_TestCase
         $this->output = new StreamOutput(fopen('php://memory', 'a', false));
     }
 
+    protected function getMultiDbConfig()
+    {
+
+        $config = new Config([
+            'paths' => [
+                'migrations' => __FILE__,
+                'seeds' => __FILE__,
+            ],
+            'environments' => [
+                'default_migration_table' => 'phinxlog',
+                'default_database' => 'development',
+                'development' => [
+                    'db1' => [
+                        'paths' => [
+                            'seeds' => __FILE__,
+                        ],
+                        'adapter' => 'mysql',
+                        'host' => 'fakehost',
+                        'name' => 'development',
+                        'user' => '',
+                        'pass' => '',
+                        'port' => 3006,
+                    ],
+                    'db2' => [
+                        'paths' => [
+                            'seeds' => __FILE__,
+                        ],
+                        'adapter' => 'mysql',
+                        'host' => 'fakehost',
+                        'name' => 'development_2',
+                        'user' => '',
+                        'pass' => '',
+                        'port' => 3006,
+                    ],
+                ],
+            ]
+        ]);
+
+        return $config;
+
+    }
+
     public function testExecute()
     {
         $application = new PhinxApplication('testing');
@@ -79,6 +121,39 @@ class StatusTest extends \PHPUnit_Framework_TestCase
                     ->will($this->returnValue(0));
 
         $command->setConfig($this->config);
+        $command->setManager($managerStub);
+
+        $commandTester = new CommandTester($command);
+        $return = $commandTester->execute(['command' => $command->getName()], ['decorated' => false]);
+
+        $this->assertEquals(0, $return);
+
+        $display = $commandTester->getDisplay();
+        $this->assertRegExp('/no environment specified/', $display);
+
+        // note that the default order is by creation time
+        $this->assertRegExp('/ordering by creation time/', $display);
+    }
+
+    public function testExecuteWithMultiDb()
+    {
+        $application = new PhinxApplication('testing');
+        $application->add(new Status());
+
+        /** @var Status $command */
+        $command = $application->find('status');
+
+        // mock the manager class
+        /** @var Manager|PHPUnit_Framework_MockObject_MockObject $managerStub */
+        $managerStub = $this->getMockBuilder('\Phinx\Migration\Manager')
+            ->setConstructorArgs([$this->getMultiDbConfig(), $this->input, $this->output])
+            ->getMock();
+        $managerStub->expects($this->exactly(2))
+                    ->method('printStatus')
+                    ->with(self::DEFAULT_TEST_ENVIRONMENT, null)
+                    ->will($this->returnValue(0));
+
+        $command->setConfig($this->getMultiDbConfig());
         $command->setManager($managerStub);
 
         $commandTester = new CommandTester($command);
@@ -120,6 +195,33 @@ class StatusTest extends \PHPUnit_Framework_TestCase
         $this->assertRegExp('/using environment development/', $commandTester->getDisplay());
     }
 
+    public function testExecuteWithEnvironmentOptionWithMultiDb()
+    {
+        $application = new PhinxApplication('testing');
+        $application->add(new Status());
+
+        /** @var Status $command */
+        $command = $application->find('status');
+
+        // mock the manager class
+        /** @var Manager|PHPUnit_Framework_MockObject_MockObject $managerStub */
+        $managerStub = $this->getMockBuilder('\Phinx\Migration\Manager')
+            ->setConstructorArgs([$this->getMultiDbConfig(), $this->input, $this->output])
+            ->getMock();
+        $managerStub->expects($this->exactly(2))
+                    ->method('printStatus')
+                    ->with('development', null)
+                    ->will($this->returnValue(0));
+
+        $command->setConfig($this->getMultiDbConfig());
+        $command->setManager($managerStub);
+
+        $commandTester = new CommandTester($command);
+        $return = $commandTester->execute(['command' => $command->getName(), '--environment' => 'development'], ['decorated' => false]);
+        $this->assertEquals(0, $return);
+        $this->assertRegExp('/using environment development/', $commandTester->getDisplay());
+    }
+
     public function testFormatSpecified()
     {
         $application = new PhinxApplication('testing');
@@ -139,6 +241,33 @@ class StatusTest extends \PHPUnit_Framework_TestCase
                     ->will($this->returnValue(0));
 
         $command->setConfig($this->config);
+        $command->setManager($managerStub);
+
+        $commandTester = new CommandTester($command);
+        $return = $commandTester->execute(['command' => $command->getName(), '--format' => 'json'], ['decorated' => false]);
+        $this->assertEquals(0, $return);
+        $this->assertRegExp('/using format json/', $commandTester->getDisplay());
+    }
+
+    public function testFormatSpecifiedWithMultiDb()
+    {
+        $application = new PhinxApplication('testing');
+        $application->add(new Status());
+
+        /** @var Status $command */
+        $command = $application->find('status');
+
+        // mock the manager class
+        /** @var Manager|PHPUnit_Framework_MockObject_MockObject $managerStub */
+        $managerStub = $this->getMockBuilder('\Phinx\Migration\Manager')
+            ->setConstructorArgs([$this->getMultiDbConfig(), $this->input, $this->output])
+            ->getMock();
+        $managerStub->expects($this->exactly(2))
+                    ->method('printStatus')
+                    ->with(self::DEFAULT_TEST_ENVIRONMENT, 'json')
+                    ->will($this->returnValue(0));
+
+        $command->setConfig($this->getMultiDbConfig());
         $command->setManager($managerStub);
 
         $commandTester = new CommandTester($command);
@@ -168,6 +297,41 @@ class StatusTest extends \PHPUnit_Framework_TestCase
         $this->config['version_order'] = \Phinx\Config\Config::VERSION_ORDER_EXECUTION_TIME;
 
         $command->setConfig($this->config);
+        $command->setManager($managerStub);
+
+        $commandTester = new CommandTester($command);
+        $return = $commandTester->execute(['command' => $command->getName()], ['decorated' => false]);
+
+        $this->assertEquals(0, $return);
+
+        $display = $commandTester->getDisplay();
+        $this->assertRegExp('/no environment specified/', $display);
+        $this->assertRegExp('/ordering by execution time/', $display);
+    }
+
+    public function testExecuteVersionOrderByExecutionTimeWithMultiDb()
+    {
+        $application = new PhinxApplication('testing');
+        $application->add(new Status());
+
+        /** @var Status $command */
+        $command = $application->find('status');
+
+        $config = $this->getMultiDbConfig();
+
+        // mock the manager class
+        /** @var Manager|PHPUnit_Framework_MockObject_MockObject $managerStub */
+        $managerStub = $this->getMockBuilder('\Phinx\Migration\Manager')
+            ->setConstructorArgs([$config, $this->input, $this->output])
+            ->getMock();
+        $managerStub->expects($this->exactly(2))
+                    ->method('printStatus')
+                    ->with(self::DEFAULT_TEST_ENVIRONMENT, null)
+                    ->will($this->returnValue(0));
+
+        $config['version_order'] = \Phinx\Config\Config::VERSION_ORDER_EXECUTION_TIME;
+
+        $command->setConfig($config);
         $command->setManager($managerStub);
 
         $commandTester = new CommandTester($command);
