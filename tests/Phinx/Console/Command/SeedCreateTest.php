f84c385b10927119008af877f095dce827e95eeb
Mike van Rooyen <michael.vanrooyen@extreme-creations.co.uk>
2017-11-23 13:57:53 +0000

    MultiDb tests implemented

--- tests/Phinx/Console/Command/SeedCreateTest.php
+++ tests/Phinx/Console/Command/SeedCreateTest.php
@@ -56,6 +56,49 @@ class SeedCreateTest extends \PHPUnit_Framework_TestCase
         $this->output = new StreamOutput(fopen('php://memory', 'a', false));
     }
 
+    protected function getMultiDbConfig()
+    {
+
+        @mkdir(sys_get_temp_dir()  . DIRECTORY_SEPARATOR . 'db1' .  DIRECTORY_SEPARATOR . 'seeds', 0777, true);
+        $config = new Config([
+            'paths' => [
+                'migrations' => sys_get_temp_dir(),
+                'seeds' => sys_get_temp_dir(),
+            ],
+            'environments' => [
+                'default_migration_table' => 'phinxlog',
+                'default_database' => 'development',
+                'development' => [
+                    'db1' => [
+                        'paths' => [
+                            'seeds' => sys_get_temp_dir()  . DIRECTORY_SEPARATOR . 'db1' . DIRECTORY_SEPARATOR . 'seeds',
+                        ],
+                        'adapter' => 'mysql',
+                        'host' => 'fakehost',
+                        'name' => 'development',
+                        'user' => '',
+                        'pass' => '',
+                        'port' => 3006,
+                    ],
+                    'db2' => [
+                        'paths' => [
+                            'seeds' => sys_get_temp_dir()  . DIRECTORY_SEPARATOR . 'db2' . DIRECTORY_SEPARATOR . 'seeds',
+                        ],
+                        'adapter' => 'mysql',
+                        'host' => 'fakehost',
+                        'name' => 'development_2',
+                        'user' => '',
+                        'pass' => '',
+                        'port' => 3006,
+                    ],
+                ],
+            ]
+        ]);
+
+        return $config;
+
+    }
+
     /**
      * @expectedException \InvalidArgumentException
      * @expectedExceptionMessage The file "MyDuplicateSeeder.php" already exists
@@ -84,6 +127,32 @@ class SeedCreateTest extends \PHPUnit_Framework_TestCase
 
     /**
      * @expectedException \InvalidArgumentException
+     * @expectedExceptionMessage The file "MyDuplicateSeeder.php" already exists
+     */
+    public function testExecuteWithMultiDb()
+    {
+        $application = new PhinxApplication('testing');
+        $application->add(new SeedCreate());
+
+        /** @var SeedCreate $command */
+        $command = $application->find('seed:create');
+
+        // mock the manager class
+        /** @var Manager|PHPUnit_Framework_MockObject_MockObject $managerStub */
+        $managerStub = $this->getMockBuilder('\Phinx\Migration\Manager')
+            ->setConstructorArgs([$this->getMultiDbConfig(), $this->input, $this->output])
+            ->getMock();
+
+        $command->setConfig($this->getMultiDbConfig());
+        $command->setManager($managerStub);
+
+        $commandTester = new CommandTester($command);
+        $commandTester->execute(['command' => $command->getName(), 'name' => 'MyDuplicateSeeder'], ['decorated' => false]);
+        $commandTester->execute(['command' => $command->getName(), 'name' => 'MyDuplicateSeeder'], ['decorated' => false]);
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
      * @expectedExceptionMessage The seed class name "badseedname" is invalid. Please use CamelCase format
      */
     public function testExecuteWithInvalidClassName()
@@ -101,6 +170,31 @@ class SeedCreateTest extends \PHPUnit_Framework_TestCase
             ->getMock();
 
         $command->setConfig($this->config);
+        $command->setManager($managerStub);
+
+        $commandTester = new CommandTester($command);
+        $commandTester->execute(['command' => $command->getName(), 'name' => 'badseedname'], ['decorated' => false]);
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     * @expectedExceptionMessage The seed class name "badseedname" is invalid. Please use CamelCase format
+     */
+    public function testExecuteWithInvalidClassNameWithMultiDb()
+    {
+        $application = new PhinxApplication('testing');
+        $application->add(new SeedCreate());
+
+        /** @var SeedCreate $command */
+        $command = $application->find('seed:create');
+
+        // mock the manager class
+        /** @var Manager|PHPUnit_Framework_MockObject_MockObject $managerStub */
+        $managerStub = $this->getMockBuilder('\Phinx\Migration\Manager')
+            ->setConstructorArgs([$this->getMultiDbConfig(), $this->input, $this->output])
+            ->getMock();
+
+        $command->setConfig($this->getMultiDbConfig());
         $command->setManager($managerStub);
 
         $commandTester = new CommandTester($command);
