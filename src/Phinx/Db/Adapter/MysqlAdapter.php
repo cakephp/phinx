d1ae1cc4ec2a572d137536d24d9e87ffc7516a35
ryoichi-u <nogami.ueno0515@gmail.com>
2017-12-09 23:06:16 +0900

    add row_format option to mysqladapter

--- src/Phinx/Db/Adapter/MysqlAdapter.php
+++ src/Phinx/Db/Adapter/MysqlAdapter.php
@@ -250,6 +250,10 @@ class MysqlAdapter extends PdoAdapter implements AdapterInterface
             $optionsStr .= sprintf(" COMMENT=%s ", $this->getConnection()->quote($options['comment']));
         }
 
+        if (isset($options['row_format'])) {
+            $optionsStr .= sprintf(" ROW_FORMAT = %s ", $options['row_format']);
+        }
+
         $sql = 'CREATE TABLE ';
         $sql .= $this->quoteTableName($table->getName()) . ' (';
         foreach ($columns as $column) {
@@ -285,6 +289,7 @@ class MysqlAdapter extends PdoAdapter implements AdapterInterface
         $sql .= ') ' . $optionsStr;
         $sql = rtrim($sql) . ';';
 
+        print_r($sql);
         // execute the sql
         $this->execute($sql);
     }
