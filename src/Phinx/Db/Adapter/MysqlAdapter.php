280b0426bbe3d07182159164cda9606002d8cdcc
stickler-ci <support@stickler-ci.com>
2018-05-16 09:14:14 +0000

    Fixing style errors.

--- src/Phinx/Db/Adapter/MysqlAdapter.php
+++ src/Phinx/Db/Adapter/MysqlAdapter.php
@@ -1032,23 +1032,23 @@ class MysqlAdapter extends PdoAdapter implements AdapterInterface
             $def .= ' `' . $index->getName() . '`';
         }
 
-	    if ( ! is_array( $index->getLimit() ) ) {
-		    $limit = '(' . $index->getLimit() . ')';
-		    $def .= ' (`' . implode( '`,`', $index->getColumns() ) . '`' . $limit . ')';
-	    } else {
-		    $columns = $index->getColumns();
-		    $limits  = $index->getLimit();
-		    $columns_count = count($columns);
-		    $def .= ' (';
-		    for ( $i = 0; $i < $columns_count; $i ++ ) {
-			    $limit = ! isset( $limits[ $i ] ) || $limits[ $i ] <= 0 ? '' : '(' . $limits[ $i ] . ')';
-			    $def .= '`' . $columns[ $i ] . '`' . $limit;
-			    if ( $i + 1 < $columns_count ) {
-				    $def .= ',';
-			    }
-		    }
-		    $def .= ' )';
-	    }
+        if (! is_array($index->getLimit())) {
+            $limit = '(' . $index->getLimit() . ')';
+            $def .= ' (`' . implode('`,`', $index->getColumns()) . '`' . $limit . ')';
+        } else {
+            $columns = $index->getColumns();
+            $limits  = $index->getLimit();
+            $columns_count = count($columns);
+            $def .= ' (';
+            for ($i = 0; $i < $columns_count; $i ++) {
+                $limit = ! isset($limits[$i]) || $limits[$i] <= 0 ? '' : '(' . $limits[$i] . ')';
+                $def .= '`' . $columns[$i] . '`' . $limit;
+                if ($i + 1 < $columns_count) {
+                    $def .= ',';
+                }
+            }
+            $def .= ' )';
+        }
 
         return $def;
     }
