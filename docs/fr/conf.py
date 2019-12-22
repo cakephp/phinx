65cfc35090c12e68bbf35058755c009f0cfad656
Mark Story <mark@mark-story.com>
2019-12-22 10:12:31 -0500

    Update import for common config.

--- docs/fr/conf.py
+++ docs/fr/conf.py
@@ -1,7 +1,7 @@
 import sys, os
 
 # Append the top level directory of the docs, so we can import from the config dir.
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath(os.path.join('..', 'config')))
 
 # Pull in all the configuration options defined in the global config file..
 from config.all import *
