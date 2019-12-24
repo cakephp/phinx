3f850ec3f7cab3be01fdf66c95978bbd17f101b3
Matthew Peveler <matt.peveler@gmail.com>
2019-12-24 12:27:54 -0800

    Remove extra semicolon

--- tests/Phinx/Config/ConfigTest.php
+++ tests/Phinx/Config/ConfigTest.php
@@ -22,7 +22,6 @@ class ConfigTest extends AbstractConfigTest
         $config = new Config([]);
         // this option is set to its default value when not being passed in the constructor, so we can ignore it
         unset($config['version_order']);
-        ;
         $this->assertAttributeEmpty('values', $config);
         $this->assertAttributeEmpty('configFilePath', $config);
         $this->assertNull($config->getConfigFilePath());
