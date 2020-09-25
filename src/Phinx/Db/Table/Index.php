5c28e4ca5d6dc0de239fd75bb6a64eabd2013b03
stickler-ci <support@stickler-ci.com>
2020-09-25 22:09:59 +0000

    Fixing style errors.

--- src/Phinx/Db/Table/Index.php
+++ src/Phinx/Db/Table/Index.php
@@ -11,197 +11,189 @@ use RuntimeException;
 
 class Index
 {
-	/**
-	 * @var string
-	 */
-	public const UNIQUE = 'unique';
+    /**
+     * @var string
+     */
+    public const UNIQUE = 'unique';
 
-	/**
-	 * @var string
-	 */
-	public const INDEX = 'index';
+    /**
+     * @var string
+     */
+    public const INDEX = 'index';
 
-	/**
-	 * @var string
-	 */
-	public const FULLTEXT = 'fulltext';
+    /**
+     * @var string
+     */
+    public const FULLTEXT = 'fulltext';
 
-	/**
-	 * @var string[]
-	 */
-	protected $columns;
+    /**
+     * @var string[]
+     */
+    protected $columns;
 
-	/**
-	 * @var string
-	 */
-	protected $type = self::INDEX;
+    /**
+     * @var string
+     */
+    protected $type = self::INDEX;
 
-	/**
-	 * @var string|null
-	 */
-	protected $name;
+    /**
+     * @var string|null
+     */
+    protected $name;
 
-	/**
-	 * @var int|array|null
-	 */
-	protected $limit;
+    /**
+     * @var int|array|null
+     */
+    protected $limit;
 
-	/**
-	 * @var string[]
-	 */
-	protected $order;
+    /**
+     * @var string[]
+     */
+    protected $order;
 
+    /**
+     * Sets the index columns.
+     *
+     * @param string[] $columns Columns
+     * @return $this
+     */
+    public function setColumns($columns)
+    {
+        $this->columns = $columns;
 
-	/**
-	 * Sets the index columns.
-	 *
-	 * @param string[] $columns Columns
-	 *
-	 * @return $this
-	 */
-	public function setColumns($columns)
-	{
-		$this->columns = $columns;
+        return $this;
+    }
 
-		return $this;
-	}
+    /**
+     * Gets the index columns.
+     *
+     * @return string[]
+     */
+    public function getColumns()
+    {
+        return $this->columns;
+    }
 
-	/**
-	 * Gets the index columns.
-	 *
-	 * @return string[]
-	 */
-	public function getColumns()
-	{
-		return $this->columns;
-	}
+    /**
+     * Sets the index type.
+     *
+     * @param string $type Type
+     * @return $this
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
 
-	/**
-	 * Sets the index type.
-	 *
-	 * @param string $type Type
-	 *
-	 * @return $this
-	 */
-	public function setType($type)
-	{
-		$this->type = $type;
+        return $this;
+    }
 
-		return $this;
-	}
+    /**
+     * Gets the index type.
+     *
+     * @return string
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
 
-	/**
-	 * Gets the index type.
-	 *
-	 * @return string
-	 */
-	public function getType()
-	{
-		return $this->type;
-	}
+    /**
+     * Sets the index name.
+     *
+     * @param string $name Name
+     * @return $this
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
 
-	/**
-	 * Sets the index name.
-	 *
-	 * @param string $name Name
-	 *
-	 * @return $this
-	 */
-	public function setName($name)
-	{
-		$this->name = $name;
+        return $this;
+    }
 
-		return $this;
-	}
+    /**
+     * Gets the index name.
+     *
+     * @return string|null
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
 
-	/**
-	 * Gets the index name.
-	 *
-	 * @return string|null
-	 */
-	public function getName()
-	{
-		return $this->name;
-	}
+    /**
+     * Sets the index limit.
+     *
+     * @param int|array $limit limit value or array of limit value
+     * @return $this
+     */
+    public function setLimit($limit)
+    {
+        $this->limit = $limit;
 
-	/**
-	 * Sets the index limit.
-	 *
-	 * @param int|array $limit limit value or array of limit value
-	 *
-	 * @return $this
-	 */
-	public function setLimit($limit)
-	{
-		$this->limit = $limit;
+        return $this;
+    }
 
-		return $this;
-	}
+    /**
+     * Gets the index limit.
+     *
+     * @return int|array
+     */
+    public function getLimit()
+    {
+        return $this->limit;
+    }
 
-	/**
-	 * Gets the index limit.
-	 *
-	 * @return int|array
-	 */
-	public function getLimit()
-	{
-		return $this->limit;
-	}
+    /**
+     * Sets the index limit.
+     *
+     * @param string[] $order
+     * @return $this
+     */
+    public function setOrder($order)
+    {
+        $this->order = $order;
 
-	/**
-	 * Sets the index limit.
-	 *
-	 * @param string[] $order 
-	 * @return $this 
-	 */
-	public function setOrder($order)
-	{
-		$this->order = $order;
+        return $this;
+    }
 
-		return $this;
-	}
+    /**
+     * Gets the index columns sort order.
+     *
+     * @return string[]
+     */
+    public function getOrder()
+    {
+        return $this->order;
+    }
 
-	/**
-	 * Gets the index columns sort order.
-	 *
-	 * @return string[]
-	 */
-	public function getOrder()
-	{
-		return $this->order;
-	}
+    /**
+     * Utility method that maps an array of index options to this objects methods.
+     *
+     * @param array $options Options
+     * @throws \RuntimeException
+     * @return $this
+     */
+    public function setOptions($options)
+    {
+        // Valid Options
+        $validOptions = ['type', 'unique', 'name', 'limit', 'order'];
+        foreach ($options as $option => $value) {
+            if (!in_array($option, $validOptions, true)) {
+                throw new RuntimeException(sprintf('"%s" is not a valid index option.', $option));
+            }
 
+            // handle $options['unique']
+            if (strcasecmp($option, self::UNIQUE) === 0) {
+                if ((bool)$value) {
+                    $this->setType(self::UNIQUE);
+                }
+                continue;
+            }
 
-	/**
-	 * Utility method that maps an array of index options to this objects methods.
-	 *
-	 * @param array $options Options
-	 *
-	 * @throws \RuntimeException
-	 *
-	 * @return $this
-	 */
-	public function setOptions($options)
-	{
-		// Valid Options
-		$validOptions = ['type', 'unique', 'name', 'limit', 'order'];
-		foreach ($options as $option => $value) {
-			if (!in_array($option, $validOptions, true)) {
-				throw new RuntimeException(sprintf('"%s" is not a valid index option.', $option));
-			}
+            $method = 'set' . ucfirst($option);
+            $this->$method($value);
+        }
 
-			// handle $options['unique']
-			if (strcasecmp($option, self::UNIQUE) === 0) {
-				if ((bool)$value) {
-					$this->setType(self::UNIQUE);
-				}
-				continue;
-			}
-
-			$method = 'set' . ucfirst($option);
-			$this->$method($value);
-		}
-
-		return $this;
-	}
+        return $this;
+    }
 }
