397f9e2e969e54b85d208f5a22ad0d46bc80061a
James Duncan <jduncan@kapitus.com>
2020-09-26 14:41:31 +0000

    fix unexpected eol

--- src/Phinx/Db/Adapter/SqlServerAdapter.php
+++ src/Phinx/Db/Adapter/SqlServerAdapter.php
@@ -1244,7 +1244,7 @@ SQL;
 		} else {
 			$indexName = sprintf('%s_%s', $tableName, implode('_', $columnNames));
 		}
-		$order = $index->getOrder()
+		$order = $index->getOrder();
 			if(!empty($order)){
 				foreach ($order as $key => $value) {
 					$loc = array_search($key, $columnNames);
