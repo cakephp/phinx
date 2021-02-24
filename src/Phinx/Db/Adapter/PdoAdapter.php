4f9df731fd195d566ccb9db8330835c281cc76f5
stickler-ci <support@stickler-ci.com>
2021-02-24 00:28:16 +0000

    Fixing style errors.

--- src/Phinx/Db/Adapter/PdoAdapter.php
+++ src/Phinx/Db/Adapter/PdoAdapter.php
@@ -193,6 +193,7 @@ abstract class PdoAdapter extends AbstractAdapter implements DirectActionInterfa
 
         $stmt = $this->getConnection()->prepare($sql);
         $result = $stmt->execute($params);
+
         return $result ? $stmt->rowCount() : $result;
     }
 
