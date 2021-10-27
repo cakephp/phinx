c0d3b9a1a111cd448b139f50984ff87291fed483
Oliver Rose <oliver@wrm-media.com>
2021-10-27 09:38:30 +0100

    Revert "Change getPhinxType to use limits for integers if detected"
    This reverts commit b791ba7b6eab2bf1824de9aada36da649884269a.

--- src/Phinx/Db/Adapter/MysqlAdapter.php
+++ src/Phinx/Db/Adapter/MysqlAdapter.php
@@ -1135,15 +1135,15 @@ class MysqlAdapter extends PdoAdapter
                 break;
             case 'tinyint':
                 $type = static::PHINX_TYPE_TINY_INTEGER;
-                $limit = $limit ?? static::INT_TINY;
+                $limit = static::INT_TINY;
                 break;
             case 'smallint':
                 $type = static::PHINX_TYPE_SMALL_INTEGER;
-                $limit = $limit ?? static::INT_SMALL;
+                $limit = static::INT_SMALL;
                 break;
             case 'mediumint':
                 $type = static::PHINX_TYPE_MEDIUM_INTEGER;
-                $limit = $limit ?? static::INT_MEDIUM;
+                $limit = static::INT_MEDIUM;
                 break;
             case 'int':
                 $type = static::PHINX_TYPE_INTEGER;
@@ -1209,7 +1209,7 @@ class MysqlAdapter extends PdoAdapter
 
         try {
             // Call this to check if parsed type is supported.
-            ['type' => $type, 'limit' => $limit] = $this->getSqlType($type, $limit);
+            $this->getSqlType($type, $limit);
         } catch (UnsupportedColumnTypeException $e) {
             $type = Literal::from($type);
         }
