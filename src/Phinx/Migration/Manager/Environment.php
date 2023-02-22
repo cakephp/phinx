eec23176b5115961759841ccc1e581c4fc3b531b
JRDuncan <duncan.jamesr@gmail.com>
2023-02-22 14:57:28 -0500

    Update Environment.php

--- src/Phinx/Migration/Manager/Environment.php
+++ src/Phinx/Migration/Manager/Environment.php
@@ -110,16 +110,13 @@ class Environment
             } else {
                 $migration->{$direction}();
             }
-	    
-	        // Record it in the database
+            // Record it in the database
             $this->getAdapter()->migrated($migration, $direction, date('Y-m-d H:i:s', $startTime), date('Y-m-d H:i:s', time()));
-
             // commit the transaction if the adapter supports it
             if ($this->getAdapter()->hasTransactions()) {
                 $this->getAdapter()->commitTransaction();
             }
         }
-
         $migration->postFlightCheck();
     }
 
