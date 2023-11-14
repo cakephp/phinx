e1be941bab6656ce0c395388bb686ec9872f88e9
Mark Scherer <dereuromark@users.noreply.github.com>
2023-11-14 14:32:38 +0100

    Fix up UUID binary16 support (#2239)
    * Fix up UUID binary16 support

--- src/Phinx/Db/Adapter/SQLiteAdapter.php
+++ src/Phinx/Db/Adapter/SQLiteAdapter.php
@@ -40,7 +40,7 @@ class SQLiteAdapter extends PdoAdapter
     protected static array $supportedColumnTypes = [
         self::PHINX_TYPE_BIG_INTEGER => 'biginteger',
         self::PHINX_TYPE_BINARY => 'binary_blob',
-        self::PHINX_TYPE_BINARYUUID => 'binary',
+        self::PHINX_TYPE_BINARYUUID => 'binary(16)',
         self::PHINX_TYPE_BLOB => 'blob',
         self::PHINX_TYPE_BOOLEAN => 'boolean_integer',
         self::PHINX_TYPE_CHAR => 'char',
