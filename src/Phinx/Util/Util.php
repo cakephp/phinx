fc08382e563d43911a5033664cae0b8261e70350
Matthew Peveler <matt.peveler@gmail.com>
2024-11-28 23:13:20 -0700

    coding style, pt 2
    Signed-off-by: Matthew Peveler <matt.peveler@gmail.com>

--- src/Phinx/Util/Util.php
+++ src/Phinx/Util/Util.php
@@ -66,7 +66,7 @@ class Util
      */
     public static function isUniqueTimestamp(string $path, string $timestamp): bool
     {
-        return !count(static::glob($path. DIRECTORY_SEPARATOR . $timestamp . '*.php'));
+        return !count(static::glob($path . DIRECTORY_SEPARATOR . $timestamp . '*.php'));
     }
 
     /**
