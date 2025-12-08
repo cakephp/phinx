a2abeeca39c053f46df7353a49c8f9a8c9b687c8
qnnp <58357109+qnnp-me@users.noreply.github.com>
2025-12-08 16:14:19 +0800

    refactor(db): Change timestamp column types to datetime - Change start_time column type to datetime - Change end_time column type to datetime

--- src/Phinx/Db/Adapter/AbstractAdapter.php
+++ src/Phinx/Db/Adapter/AbstractAdapter.php
@@ -310,8 +310,8 @@ abstract class AbstractAdapter implements AdapterInterface
             $table = new Table($this->getSchemaTableName(), $options, $this);
             $table->addColumn('version', 'biginteger', ['null' => false])
                 ->addColumn('migration_name', 'string', ['limit' => 100, 'default' => null, 'null' => true])
-                ->addColumn('start_time', 'timestamp', ['default' => null, 'null' => true])
-                ->addColumn('end_time', 'timestamp', ['default' => null, 'null' => true])
+                ->addColumn('start_time', 'datetime', ['default' => null, 'null' => true])
+                ->addColumn('end_time', 'datetime', ['default' => null, 'null' => true])
                 ->addColumn('breakpoint', 'boolean', ['default' => false, 'null' => false])
                 ->save();
         } catch (Exception $exception) {
